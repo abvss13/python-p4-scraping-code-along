619ccd037a6be1fda603000bda0cfcc9100bacb4
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-01-13 12:54:18 +0300

    done

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,8 +1,11 @@
-from bs4 import BeautifulSoup
-import requests
-from Course import Course
-import ipdb
+class Course:
+    def __init__(self, title='', schedule='', description=''):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
 
-
-class Scraper:
-    pass
+    def __str__(self):
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '------------------'
+        return output
